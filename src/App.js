dc3fdf6c1fe79155a3e0dc88ea3e889de2e035dd
benjatestaferri7 <benjamintestaferrig@gmail.com>
2021-03-19 17:05:41 -0300

    add localstorage (not working yet)

--- src/App.js
+++ src/App.js
@@ -5,27 +5,40 @@ import SearchBar from './Components/SearchBar';
 import Pokedex from './Components/Pokedex';
 import { getPokemons, getPokemonsData } from './api';
 import { FavoriteProvider } from './Context/favoritesContext';
+import Footer from './Components/Footer';
+
+const localStorageKey = 'favorite_pokemon';
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
   const [total, setTotal] = useState(0);
   const [loading, setLoading] = useState(true);
-  const [favorites, setFavorites] = useState(["charizard"]);
+  const [favorites, setFavorites] = useState([]);
 
   const fetchPokemons = async () => {
     try {
       setLoading(true);
-      const data = await getPokemons(25, 25 * page);
+      const data = await getPokemons(12, 12 * page);
       const promises = data.results.map(async (pokemon) => {
         return await getPokemonsData(pokemon.url);
       });
       const results = await Promise.all(promises);
       setPokemons(results);
       setLoading(false);
-      setTotal(Math.ceil(data.count / 25));
+      setTotal(Math.ceil(data.count / 12));
     } catch (error) {}
   };
+
+  const loadFavoritePokemons = () => {
+    const pokemons =
+      JSON.parse(window.localStorage.getItem(localStorageKey)) || [];
+    setFavorites(pokemons);
+  };
+
+  useEffect(() => {
+    loadFavoritePokemons();
+  }, []);
 
   useEffect(() => {
     fetchPokemons();
@@ -33,26 +46,28 @@ function App() {
 
   const updateFavoritePokemons = (name) => {
     const updated = [...favorites];
-    
-   const isFavorite = updated.indexOf(name);
 
-    if(isFavorite >= 0) {
+    const isFavorite = updated.indexOf(name);
+
+    if (isFavorite >= 0) {
       updated.splice(isFavorite, 1);
     } else {
       updated.push(name);
     }
     setFavorites(updated);
-
-  }
+  };
 
   return (
     <FavoriteProvider
-      value={{ favoritePokemons: favorites,updateFavoritePokemons: updateFavoritePokemons }}
+      value={{
+        favoritePokemons: favorites,
+        updateFavoritePokemons: updateFavoritePokemons
+      }}
     >
       <div>
         <Navbar />
         <div className="App">
-          <SearchBar />(
+          <SearchBar />
           <Pokedex
             loading={loading}
             pokemons={pokemons}
@@ -61,6 +76,7 @@ function App() {
             total={total}
           />
         </div>
+        <Footer />
       </div>
     </FavoriteProvider>
   );
---
+++ src/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <div>&copy; Benjamín Testaferri Gomez</div>
+    <div>
+      <a
+        href="https://btgportfolio.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        💻 Mi Portfolio
+      </a>
+    </div>
+  </footer>
+);
+
+export default Footer;
